f8067a1ed625ece8d35884110d28949c37bd6557
relinkz <seb.lundgren@gmail.com>
2015-11-27 13:37:43 +0100

    testFil
    Ser du detta axel?

---
+++ block.h
@@ -0,0 +1,50 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Block
+{
+private:
+    char *block;
+    int nrOfElements;
+
+public:
+    /* Constructor */
+    Block(int nrOfElements = 512);    // overloaded (default) constructor
+    Block(const Block &other); // copy-constructor
+
+    /* Destructor */
+    ~Block();
+
+    /* Operators */
+    Block& operator = (const Block &other);  // Assignment operator
+    char operator[] (int index) const;  // []-operator
+    friend std::ostream& operator<<(std::ostream &os, const Block& blck)
+    {
+        for (int i = 0; i < blck.nrOfElements; ++i)
+            os << blck.block[i];
+        return os;
+    }
+
+    void reset(char c = 0);  // Sets every element in char-array to 0
+    int size() const;   // returns the size
+    Block readBlock() const;    // Returns a copy of block
+
+    /* Write a block */
+    int writeBlock(const std::string &strBlock);
+    int writeBlock(const std::vector<char> &vec);
+    void writeBlock(const char cArr[]);     // Use with caution! Make sure that cArr is at least as large as private member block.
+
+	/* Appending and editing a block */
+	int findEmptySpace(const string &dataToAdd) const;
+	void appendToBlock(const int &pos, const string &dataToAdd);
+
+    std::string toString() const;
+};
+
+#endif // BLOCK_H
